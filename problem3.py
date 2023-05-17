8d3e47f533c2cdcd588d46c0a170655df6ce0cfe
Bushra alalawi <133573946+Bush79ar@users.noreply.github.com>
2023-05-17 03:02:31 -0700

    p3

---
+++ problem3.py
@@ -0,0 +1,16 @@
+
+list = [1,2,7,5,8,2,1]
+new = []  
+for a in list: 
+    n = list.count(a)
+    if n == 1:
+        #if new.count(a) == 0:  
+            new.append(a)
+            new1= new[0]
+    
+            
+           
+print(new1)
+
+
+
