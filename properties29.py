11db3bc8435138079784eeebc9f74a366c4c1b5e
Bushra alalawi <133573946+Bush79ar@users.noreply.github.com>
2023-05-30 11:50:20 -0700

    29/5/2023

---
+++ properties29.py
@@ -0,0 +1,14 @@
+def getMultiples(num, number_of_multiples):
+	for i in range(1, (number_of_multiples+1)):
+		print(num*i)
+
+
+getMultiples(3, 7);
+
+
+def getMultiples(num, number_of_multiples):
+	for i in range(1, (number_of_multiples+1)):
+		print(num*i)
+
+
+getMultiples(10, 4);
