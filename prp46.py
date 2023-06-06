6247c61e34165c3d67b1fec89842919287118c9a
Bushra alalawi <133573946+Bush79ar@users.noreply.github.com>
2023-06-06 03:10:50 -0700

    Delete prp46.py

--- prp46.py
+++
@@ -1,9 +0,0 @@
-sentence = input("Enter sentence:")
-
-words = sentence.split()
-if "CodeAcademy" in words:
-    pos = words.index("CodeAcademy")
-    print("I found CodeAcademy at",pos)
-
-else:
-    print("I didnt find CodeAcademy ")
